2b310e517a9867117d2597922b1ac32d05975bf6
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-03-17 16:06:55 -0700

    Implement character counter, turns red at <0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
   console.log($("#tweet-text:focus"));
   $("#tweet-text").on("input", function() {
+    const counter = $(this).siblings(".footer").children()[0];
+
     const maxChars = 140;
     let charCount = this.value.length;
-    let counter = maxChars - charCount;
+    let currentCount = maxChars - charCount;
+    
+    if (currentCount < 0) {
+      counter.style.color = "red";
+    } else {
+      counter.style.color = "maroon";
+    }
+    //Update counter in HTML/CSS
+    counter.value = currentCount;
   });
 });
