24208a1838d0926d1f37ae3b9cc6e734a4573263
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-03-23 19:24:05 -0700

    AJAX call tweets, use timeago library to convert timestamps

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,7 +5,7 @@
  */
 
 $(document).ready(function() {
-  console.log('ready');
+  console.log('jQuery ready');
   const createTweetElement = function(tweetObject) {
     return $(`
     <article class="tweet">
@@ -21,7 +21,7 @@ $(document).ready(function() {
           <p>${tweetObject.content.text}</p>
         </section>
         <footer>
-          <span class="date">${tweetObject["created_at"]}</span>
+          <span class="date">${timeago.format(tweetObject["created_at"])}</span>
           <span class="icons-box"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i></span>
         </footer>
       </article>
@@ -30,41 +30,64 @@ $(document).ready(function() {
 
 
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
+  let tweetData;
 
-
+  const loadTweets = function() {
+    $.ajax('/tweets', { method: 'GET' })
+    .then(function(data) {
+      console.log(`success: received tweetData ${data}`);
+      renderTweets(data);
+    });
+  };
 
   const renderTweets = function(tweetArray) {
     let $tweet;
     for (const tweetObject of tweetArray) {
       $tweet = createTweetElement(tweetObject);
-      $('.tweet-box').append($tweet);
+      $('.tweet-box').prepend($tweet);
     }
   };
 
+  loadTweets();
+  // renderTweets(data);
 
-  renderTweets(data);
+  $('#submit-form').on('submit', function(event) {
+    $.ajax('/tweets.html', { method: 'POST' });
+    event.preventDefault();
+    console.log('preventing default');
+    const $data = $(this).serialize();
+
+    $.post('/tweets', $data, console.log('Post to /tweets success'));
+
+  });
+  // $('#submit-tweet').on('click', function(event) {
+  //   $.ajax('/tweets.html', { method: 'POST' });
+  //   event.preventDefault();
+  // });
+
+
 });
