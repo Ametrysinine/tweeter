43ce12c2a43e7dcac728eb0cef6f0801e2a4f817
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-03-23 18:39:24 -0700

    Tweets render dynamically

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -30,21 +30,41 @@ $(document).ready(function() {
 
 
 
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+
+
+  const renderTweets = function(tweetArray) {
+    let $tweet;
+    for (const tweetObject of tweetArray) {
+      $tweet = createTweetElement(tweetObject);
+      $('.tweet-box').append($tweet);
+    }
   };
 
-  const $tweet = createTweetElement(tweetData);
 
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('.tweet-box').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  renderTweets(data);
 });
