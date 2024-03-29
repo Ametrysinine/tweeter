29a02bb6c0c52c9686b2a99a4f5c38f561c8b941
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-03-29 12:50:51 -0700

    Temp commit before revert

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
     return div.innerHTML;
   };
 
-
+  
   const createTweetElement = function(tweetObject) {
     return $(`
     <article class="tweet">
@@ -37,6 +37,33 @@ $(document).ready(function() {
     `);
   };
 
+
+
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
+
   const loadTweets = function() {
     $('.tweet-box').empty();
     $.ajax('/tweets', { method: 'GET', success: function(data) { renderTweets(data); }, });
@@ -52,10 +79,12 @@ $(document).ready(function() {
 
   loadTweets();
 
+
   $('#submit-form').on('submit', function(event) {
     error.style.display = 'none';
 
     const textSubmission = document.querySelector('#new-tweet-text').value.trim();
+
     if (textSubmission === "" || textSubmission === null) {
       $('#error').slideDown('slow');
       error.innerHTML = 'Your tweet is blank.';
@@ -69,6 +98,7 @@ $(document).ready(function() {
     }
 
     let $data = $(this).serialize();
+    console.log($data);
     $.post('/tweets', $data, loadTweets);
 
     $('#new-tweet-text').val("");
